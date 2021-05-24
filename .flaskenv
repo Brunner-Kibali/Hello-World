d19a1499faec12e748f210707788f4fc50b1aafa
Brunner <brunnerkibali@gmail.com>
2021-05-23 23:40:27 -0700

    Adding initial simple Flask project files

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=HelloWorld
+FLASK_ENV=development
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='HelloWorld',
+    packages=['HelloWorld'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
